8b0ca9d4023e8f4961cb24aa43cfe332323a40c4
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-06-26 08:47:25 +0000

    provided database

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const cors = require('cors');
 const experss = require('express');
 const movies = require('./Movie data/data.json');
 const axios = require('axios');
+const pg = require('pg');
 
 const app = experss();
-const port = 3000;
 require("dotenv").config();
 app.use(cors());
 app.use(experss.static(__dirname + '/public'));
+app.use(experss.json());
 
+const client = new pg.Client(process.env.DATABASE_URL + process.env.DATABASE_NAME);
 // routes
 
 app.get('/', (req, res) => {
@@ -38,6 +40,48 @@ app.get('/favorite/:id', (req, res) => {
         })
     } else res.send(`ID is ${id}`);
 })
+
+// DataBase
+
+app.get('/getmovies', (req, res) => {
+    const sql = `SELECT * from movie`;
+    client.query(sql).then((data) => {
+        res.status(200).send(data.rows);
+    })
+})
+
+app.get('/searchmovies', (req, res) => {
+    const { query } = req;
+    if (Object.keys(query).length === 0 && query.constructor === Object) {
+        res.status(400).send(`You should provide some query strings`);
+    } else {
+        const sql = `SELECT * from movie where title = '${query.title}'`;
+        client.query(sql).then((data)=>{
+            data.rowCount > 0 ? res.status(200).send(data.rows) : res.status(200).send(`No such Movie like ${query.title}`);
+        }).catch((err)=>{
+            res.status(500).send(`An Error Occurd while trying to search`)
+            console.log(`Error in search`,err);
+        })
+    }
+})
+
+app.post('/addmovies', (req, res) => {
+    const { title, lang, type } = req.body;
+    const sql = `INSERT INTO movie (title, lang, type) VALUES ($1, $2, $3) ON CONFLICT (title) DO NOTHING`;
+    client.query(sql, [title, lang, type]).then((data) => {
+        if (data.rowCount > 0) {
+            res.status(201).send(`Movie ${title} has been successfully Added`);
+        } else {
+            res.status(409).send(`Movie ${title} already exists`);
+        }
+    }).catch((err) => {
+        res.status(500).send('An error occurd while adding the movie');
+        console.log('Error Adding movie', err);
+    })
+})
+
+
+// third party API
 
 app.get('/trending', async (req, res) => {
     const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}&language=en-US`
@@ -89,4 +133,6 @@ function Movie(title, poster_path, overview) {
     this.poster_path = poster_path;
     this.overview = overview;
 }
-app.listen(port, () => { console.log("started on port 3000") })
+client.connect().then(() => {
+    app.listen(process.env.PORT, () => { console.log("started on port 3000") })
+})
